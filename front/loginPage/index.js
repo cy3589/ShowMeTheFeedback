ce92ba66529dac3f668741db166938f4bf13b360
정경훈 <ruderud0055@gmail.com>
2021-12-22 12:14:04 +0900

    페이지 연결

--- front/loginPage/index.js
+++ front/loginPage/index.js
@@ -1,1 +1,7 @@
 const inputEmail = document.getElementsByClassName("");
+
+const logInBtn = document.querySelector(".loginButton");
+logInBtn.addEventListener("click", () => {
+  history.pushState({ data: "fetchData" }, null, "../Projects");
+  location.reload();
+});

0b4bbfa4cd8a0208131131bbe38d5c52e2d1f40c
정경훈 <ruderud0055@gmail.com>
2021-12-22 15:57:02 +0900

    myPage 베이스로직 작성완료

--- front/myPage/index.js
+++ front/myPage/index.js
@@ -1,6 +1,38 @@
 const submitBtn = document.querySelector(".my-page__submit-button");
+const myPageInputs = document.getElementsByTagName("input");
+
+let isEqual = false;
+
+myPageInputs["passwordConfirm"].addEventListener("input", () => {
+  if (
+    myPageInputs["password"].value !== myPageInputs["passwordConfirm"].value
+  ) {
+    document.querySelector(
+      ".my-page__new-password-confirm-input label"
+    ).innerHTML = "위와 다릅니다";
+    isEqual = false;
+  } else {
+    document.querySelector(
+      ".my-page__new-password-confirm-input label"
+    ).innerHTML = "위와 일치합니다!";
+    isEqual = true;
+  }
+});
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log(e);
+
+  if (!isEqual) {
+    alert("비밀번호를 다시 확인해주세요!");
+    return;
+  }
+
+  const myPageValues = {
+    email: myPageInputs["email"].value,
+    nickName: myPageInputs["nickName"].value,
+    password: myPageInputs["password"].value,
+  };
+
+  //myPageValues를 fetch
+  console.log(myPageValues);
 });

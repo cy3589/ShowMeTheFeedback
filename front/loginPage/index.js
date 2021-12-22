a55ca2a4ba2dc4a62984f1b1990e0fca86ce9969
정경훈 <ruderud0055@gmail.com>
2021-12-22 16:48:20 +0900

    loginTest

--- front/loginPage/index.js
+++ front/loginPage/index.js
@@ -1,7 +1,32 @@
-const inputEmail = document.getElementsByClassName("");
+// const inputEmail = document.getElementsByClassName("");
+
+// const logInBtn = document.querySelector(".loginButton");
+// logInBtn.addEventListener("click", () => {
+//   history.pushState({ data: "fetchData" }, null, "../Projects");
+//   location.reload();
+// });
+
+const baseURL = "elice-kdt-sw-1st-vm05.koreacentral.cloudapp.azure.com:5000";
 
 const logInBtn = document.querySelector(".loginButton");
-logInBtn.addEventListener("click", () => {
-  history.pushState({ data: "fetchData" }, null, "../Projects");
-  location.reload();
+
+logInBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const emailValue = document.querySelector(".emailInput").value;
+  const passwordValue = document.querySelector(".passwordInput").value;
+
+  const config = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(config);
+
+  const response = await fetch(`${baseURL}/api/auth/login`, {
+    method: "POST",
+    body: config,
+  });
+
+  console.log(response.data);
 });

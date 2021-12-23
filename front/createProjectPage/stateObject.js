99f3a6d918b3630d7fc6b17fccbb27c3337b8c36
cy3589 <jcy3589@naver.com>
2021-12-23 14:09:51 +0900

    createProject_fetchURL,header

--- front/createProjectPage/stateObject.js
+++ front/createProjectPage/stateObject.js
@@ -4,7 +4,6 @@ export const stateObject = {
   mainFunc: "",
   skills: "",
   member: [{ name: "", job: "", task: "" }],
-  teamDiscribe: "",
   thumbnails: [], // 미리보기 구현을 위한 state(post요청 시 포함하지 않음)
   postThumbnailsData: [], // post요청을 위한 state
 };
--- front/createProjectPage/submitFunc.js
+++ front/createProjectPage/submitFunc.js
@@ -1,6 +1,7 @@
 import { stateObject } from "./stateObject.js";
-
-export const submitFunc = () => {
+import { getTokenFromCookies } from "../auth/token.js";
+export const submitFunc = (e) => {
+  e.preventDefault();
   const countFalseIcon = document.querySelectorAll(".check-false").length;
   if (countFalseIcon >= 1) {
     alert("모든 칸을 작성해주세요");
@@ -13,7 +14,6 @@ export const submitFunc = () => {
     mainFunc,
     skills,
     member,
-    teamDiscribe,
     postThumbnailsData,
   } = stateObject;
   formData.append("teamName", teamName);
@@ -21,34 +21,27 @@ export const submitFunc = () => {
   formData.append("mainFunc", mainFunc);
   formData.append("skills", skills);
   formData.append("member", JSON.stringify(member));
-  formData.append("teamDiscribe", teamDiscribe);
   for (let i = 0; i < postThumbnailsData.length; i++) {
     formData.append("thumbnails", postThumbnailsData[i]);
   }
   const options = {
     method: "POST",
     body: formData,
+    headers: {
+      access: getTokenFromCookies(accessToken),
+    },
   };
-  fetch("http://localhost:8080/api/post/project", options).then((result) => {
-    // console.log(result);
-    // if (result.success === "true") {
-    //   alert("등록되었습니다.")
-    //   window.location.href = `/project/${result.projectId}`;
-    // } else {
-    //   alert("게시글 등록에 실패하였습니다. 다시 시도해주세요");
-    // }
-  });
-
-  //게시글작성 후 작성글 페이지로 이동(임시)
-  alert("등록되었습니다.");
-  history.pushState({ data: "fetchData" }, null, "../reviewPage/static"); //실제 url = /project/projectId123 => fetch res.data.projectId
-  location.reload();
-
-  // const option = {
-  //   method: "post",
-  //   body: JSON.stringify({ a: 123, b: 456 }),
-  // };
-  // fetch("http://localhost:8080/api/post/project", option);
-  return false;
-  // return true;
+  fetch(
+    "http://elice-kdt-sw-1st-vm05.koreacentral.cloudapp.azure.com:5000/api/projects",
+    options
+  )
+    .then((result) => result.json())
+    .then((result) => {
+      if (result.message) {
+        alert("프로젝트를 생성했습니다.");
+        window.location.href(`/project/${result.projectId}`);
+      } else {
+        alert(result.error);
+      }
+    });
 };

7eea21840298a94b044ea92b288cd713cce5866c
정경훈 <ruderud0055@gmail.com>
2021-12-23 09:49:27 +0900

    회원가입후 로그인페이지 이동기능추가

--- front/signUpPage/index.js
+++ front/signUpPage/index.js
@@ -48,79 +48,13 @@ submitBtn.addEventListener("click", (e) => {
     passwordInput.value,
     signUpCodeInput.value
   );
+
+  if (status === 201) {
+    alert("가입 완료되었습니다.");
+    history.pushState({ data: data }, null, "../loginPage");
+    location.reload();
+    return;
+  }
   console.log(data, status);
+  alert("다시 시도해주세요");
 });
-
-// const writeTimer = (timer) => {
-//   getSignUpCodeButton.innerText = `코드인증\n${Math.floor(timer / 60)} : ${
-//     String(timer % 60).length === 2 ? timer % 60 : "0" + String(timer % 60)
-//   }`;
-// };
-
-// getSignUpCodeButton.addEventListener("click", (e) => {});
-//       submitBtn.style.backgroundColor = "#ff6b6b";
-
-// const startTimer = (timer) => {
-//   setTimeout(() => {
-
-//     timer -= 1;
-//     if (timer > 0) {
-//       writeTimer(timer);
-
-//       console.log(timer);
-
-//       getSignUpCodeButton.addEventListener("click", (e) => {
-//         e.preventDefault();
-//         clearTimeout(startTimer());
-//         return;
-//       });
-
-//       startTimer(timer);
-//     } else {
-//       console.log(timer);
-
-//       getSignUpCodeButton.innerText = "인증코드재발송";
-//       getSignUpCodeButton.style.color = "black";
-//       submitBtn.style.backgroundColor = "#bbbbbb";
-//     }
-//   }, 1000 * timer);
-// };
-
-// getSignUpCodeButton.addEventListener("click", (e) => {
-//   let timer = 5;
-
-//   if (
-//     getSignUpCodeButton.innerText === "인증코드발송" ||
-//     getSignUpCodeButton.innerText === "인증코드재발송"
-//   ) {
-//     e.preventDefault();
-//     alert(
-//       `이메일로 인증코드가 발급되었습니다.\n시간내에 코드를 입력하고 코드인증을 클릭하세요.`
-//     );
-//     let timer = 180;
-//     startTimer(timer);
-
-//     signUpCodeInput.style.backgroundColor = "white";
-//     signUpCodeInput.disabled = false;
-
-//     getSignUpCodeButton.style.color = "red";
-//   } else {
-//     const isPass = true; //confirmSignUpCode() => 코드유효확인
-//     if (isPass) {
-//       e.preventDefault();
-//       timer = 0;
-//       clearTimeout(startTimer);
-//       alert("확인되었습니다. 비밀번호를 입력해주세요");
-//       getSignUpCodeButton.innerText = "확인완료!";
-//     } else {
-//       alert("코드를 다시 확인해주세요");
-//     }
-//   }
-// });
-
-// submitBtn.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   if (getSignUpCodeButton.innerText === "인증코드발송") {
-//     alert("인증코드를 ");
-//   }
-// });

78ae787f1f0c8226da758e6c78fb0e005bb23101
정경훈 <ruderud0055@gmail.com>
2021-12-24 12:46:25 +0900

    로그인페이지 콘솔로그, 계정기본값 삭제

--- front/loginPage/index.js
+++ front/loginPage/index.js
@@ -3,10 +3,6 @@ import { saveToken } from "../auth/token.js";
 
 const logInBtn = document.querySelector(".loginButton");
 
-//디버깅 편의상 아이디/비밀번호 기입력
-document.querySelector(".emailInput").value = "ansrud45@gmail.com";
-document.querySelector(".passwordInput").value = "iw92qt2xl";
-
 logInBtn.addEventListener("click", async (e) => {
   e.preventDefault();
 
@@ -19,15 +15,12 @@ logInBtn.addEventListener("click", async (e) => {
     for (let key in data) {
       saveToken(key, data[key]);
     }
-    history.pushState({ data: data }, null, "../Projects"); //실제 url = /project/projectId123 => fetch res.data.projectId
+    history.pushState({ data: data }, null, "../Projects");
     location.reload();
   } else {
     alert("이메일 또는 비밀번호를 확인해주세요");
   }
 });
-
-// const baseURL =
-//   "http://elice-kdt-sw-1st-vm05.koreacentral.cloudapp.azure.com:5000";
 
 const logInBtnByGoogle = document.querySelector(".loginByGoogleButton");
 
@@ -38,5 +31,4 @@ logInBtnByGoogle.addEventListener("click", async (e) => {
       method: "GET",
     }
   );
-  console.log(res.data);
 });

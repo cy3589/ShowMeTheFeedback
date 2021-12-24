948fe7af71534b709e1eb9941ee003c7409e4429
정경훈 <ruderud0055@gmail.com>
2021-12-24 12:53:38 +0900

    naviBar 콘솔로그 삭제

--- front/naviBar/index.js
+++ front/naviBar/index.js
@@ -4,21 +4,19 @@ import { unauthorizedNaviBar } from "./unauthorizedNavibar.js";
 import { refreshToken as ref } from "../api/refreshToken.js";
 import { logOut } from "../auth/logOut.js";
 globalThis.addEventListener("load", async () => {
-  const refreshToken = getTokenFromCookies("refreshToken"); //먼저 ref토큰이 살아있는지 확인하고
-  const isAuthed = refreshToken !== undefined ? true : false; //살아있다면
-  isAuthed ? authorizedNaviBar() : unauthorizedNaviBar(); //탑네비를 먼저 렌더링 시킨다
+  const refreshToken = getTokenFromCookies("refreshToken");
+  const isAuthed = refreshToken !== undefined ? true : false;
+  isAuthed ? authorizedNaviBar() : unauthorizedNaviBar();
 
-  const accessToken = getTokenFromCookies("accessToken"); //이후 acc토큰이 유효한지 확인하고
-  const accExpired = accessToken === undefined ? true : false; //만료되었다면
+  const accessToken = getTokenFromCookies("accessToken");
+  const accExpired = accessToken === undefined ? true : false;
   if (accExpired) {
     const res = await ref();
     if (res.status === 200) {
       for (let key in data) {
         saveToken(key, data[key]);
       }
-      console.log("tokenRefresh!", res); //acc, ref토큰을 리프레시받는다
     }
-    console.log("refresh에 문제가 있습니다!", res);
   }
 
   const logOutBtn = document.querySelector(".naviBar__logOut");
